a09dac1097955ba5dfbdda30eb1c0f180c13d803
simona.loperfido <sloperfido96@gmail.com>
2021-09-15 16:39:29 +0200

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,4 +29,18 @@ export class AppComponent {
       priority: 5 
     }
   ];
+
+
+  addTodo(newTodoLabel: any) {
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo: any) {
+    this.todos = this.todos.filter(t => t.label !== todo.label);
+  }
 }
